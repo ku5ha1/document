7d70a706cef70ae21a2021add0662a9580918bfe
kushalraga <kushalraga20@gmail.com>
2025-09-02 18:12:17 +0530

    reset

--- app/models/tags.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, Integer
-from app.utils.database import Base 
-
-class Tag(Base):
-    __tablename__ = "tags"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
